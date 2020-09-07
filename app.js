2c8808d2a9e9a3f61c0d977a43a305f4458d53ee
francois <contact.fpweb@gmail.com>
2020-09-08 02:15:56 +0900

    Add ui colors

---
+++ app.js
@@ -0,0 +1,61 @@
+// global var
+const colorDivs = document.querySelectorAll(".color");
+const generateBtn = document.querySelector(".generate");
+const sliders = document.querySelectorAll('input[type="range]');
+const currentHexes = document.querySelectorAll(".color h2");
+let initialColors;
+
+//functions
+function generateHex() {
+	const hexColor = chroma.random();
+	return hexColor;
+}
+
+function randomColors() {
+	colorDivs.forEach((div, index) => {
+		const hextText = div.children[0];
+		const randomColor = generateHex();
+		div.style.background = `${randomColor}`;
+		hextText.innerText = randomColor;
+		// check contrast
+		checkTextContrast(randomColor, hextText);
+		// inital colorize Sliders
+		const color = chroma(randomColor);
+		const sliders = div.querySelectorAll(".sliders input");
+		const hue = sliders[0];
+		const brightness = sliders[1];
+		const saturation = sliders[2];
+
+		colorizeSliders(color, hue, brightness, saturation);
+	});
+}
+
+function checkTextContrast(color, text) {
+	const luminance = chroma(color).luminance();
+	if (luminance > 0.5) {
+		text.style.color = "black";
+	} else {
+		text.style.color = "white";
+	}
+}
+
+function colorizeSliders(color, hue, brightness, saturation) {
+	// scale saturation
+	const noSat = color.set("hsl.s", 0);
+	const fullSat = color.set("hsl.s", 1);
+	const scaleSat = chroma.scale([noSat, color, fullSat]);
+	// scale brightness
+	const midBright = color.set("hsl.l", 0.5);
+	const scaleBright = chroma.scale(["black", midBright, "white"]);
+
+	// input update color
+	saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSat(
+		0
+	)}, ${scaleSat(1)})`;
+	brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBright(
+		0
+	)},${scaleBright(0.5)}, ${scaleBright(1)})`;
+	hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75),rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))`;
+}
+
+randomColors();
